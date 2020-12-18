877a7975ed03192e33e9492ce4e4886018f0ec3a
faizan <faizan.ah862@gmail.com>
2020-12-18 18:14:54 +0500

    added dummy landing page

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import LandingPageHeader from './Components/LandingPageHeader'
+import './StyleSheets/LandingPage.css'
+
 class LandingPage extends React.Component{
     onClickLogin = ()=>{
         
@@ -7,9 +10,12 @@ class LandingPage extends React.Component{
     render(){
         return(
             <div className="landing-page-main">
-                <h1>Landing Page</h1>
-                <Link to="/login"><button>login</button></Link>
-                <Link to="/registration"><button>signup</button></Link>
+                <div>
+                    <LandingPageHeader/>
+                </div>
+                <div className="landing-heading">
+                    <h1>WELCOME TO INVESCO</h1>
+                </div>
             </div>
         )
     }
